f746ba1dd40c3ad70099cf60d5dcf78a6ce94f38
Skyzary <132926053+Skyzary@users.noreply.github.com>
2025-12-03 23:14:46 +0100

    goit hw 09 update 2-form functionality and package.json adjustments

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,4 @@
+
 let formDate = {
   email: '',
   message: '',
@@ -6,6 +7,7 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', (event) =>{
   formDate[event.target.name] = event.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formDate));
+  console.log(localStorage.getItem('feedback-form-state'));
 })
 form.addEventListener('submit', (event) =>{
   event.preventDefault();
@@ -16,4 +18,6 @@ form.addEventListener('submit', (event) =>{
   }
   else alert('all fields must be filled')
 })
-
+const {email, message } = JSON.parse(localStorage.getItem('feedback-form-state'));
+form.email.value = email;
+form.message.value = message;
