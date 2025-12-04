3fb64ea92a478dbbb79ec0b5321cddd3f185cd09
Skyzary <132926053+Skyzary@users.noreply.github.com>
2025-12-04 13:59:57 +0100

    goit hw 09 fix 2-form localStorage handling and data persistence

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,23 +1,26 @@
 
-let formDate = {
+let formData = {
   email: '',
   message: '',
 }
+const {email, message } = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (localStorage.getItem('feedback-form-state') === null){
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
 const form = document.querySelector('.feedback-form');
+form.email.value = email;
+form.message.value = message;
 form.addEventListener('input', (event) =>{
-  formDate[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formDate));
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
   console.log(localStorage.getItem('feedback-form-state'));
 })
 form.addEventListener('submit', (event) =>{
   event.preventDefault();
   if (event.target.email.value !== '' && event.target.message.value !== '') {
     form.reset();
-    console.log(localStorage.getItem('feedback-form-state'));
-    localStorage.clear()
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
   }
   else alert('all fields must be filled')
 })
-const {email, message } = JSON.parse(localStorage.getItem('feedback-form-state'));
-form.email.value = email;
-form.message.value = message;
